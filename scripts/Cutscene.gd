fa85cfb26faa369c18576e85456e34028e3b55bd
Panda248 <p4nda248@gmail.com>
2023-07-08 16:00:42 -0400

    cutscenes

--- scripts/Cutscene.gd
+++ scripts/Cutscene.gd
@@ -1,11 +1,28 @@
 extends Control
 
-
+var index = 0
+var total_actions = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	total_actions = get_child_count()-1
+	print(str(total_actions))
+	for i in range(get_child_count(true)-1):
+		get_child(i+1).hide()
+	
 	pass # Replace with function body.
 
+func start():
+	show()
+	if(get_node("Background").has_node("AnimationPlayer")):
+		get_node("Background").get_node("AnimationPlayer").play("action")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func proceed():
+	self.index+=1
+	print(str(index))
+	if(get_child(index)):
+		get_child(index).show()
+		get_child(index).get_child(0).play("action")
 	pass
+
+func is_finished() -> bool:
+	return index == total_actions
--- scripts/CutsceneManager.gd
+++ scripts/CutsceneManager.gd
@@ -2,17 +2,32 @@ extends CanvasLayer
 
 var sceneList
 var currentSceneIndex = 0
+var sceneCount = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sceneList = $Scenes
+	sceneCount = sceneList.get_children().size()-1
+	for i in range(sceneCount-1):
+		sceneList.get_child(i+1).hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_just_released("ui_accept")):
-		proceed()
-	pass
+		if(sceneCount == currentSceneIndex):
+			exit()
+		else:
+			proceed()
 
 func proceed():
+	if(sceneList.get_child(currentSceneIndex).is_finished()):
+		sceneList.get_child(currentSceneIndex).hide()
+		currentSceneIndex+=1
+		sceneList.get_child(currentSceneIndex).start()
+	else:
+		sceneList.get_child(currentSceneIndex).proceed()
+ 
+func exit():
+	get_tree().quit()
 	pass
