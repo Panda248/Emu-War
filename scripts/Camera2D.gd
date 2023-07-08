a9c46d68bfaecb6cda8f4f9070e85ca389e42814
Lux <64970263+lumx7@users.noreply.github.com>
2023-07-08 11:45:33 -0400

    zoomplus

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -8,7 +8,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	var lerp_amount = 0.1
+	var speed = 0.01
 	var offset = (get_global_mouse_position()-get_parent().player.global_position)*0.3
-	global_position = get_parent().player.global_position + offset
+	
+	var distance = get_parent().player.global_position.distance_to(global_position)
+	
+	if get_parent().player.global_position != global_position: global_position = global_position.move_toward(get_parent().player.global_position, distance/10)
+	
+	
+	#global_position = get_parent().player.global_position + offset
 
 	pass
