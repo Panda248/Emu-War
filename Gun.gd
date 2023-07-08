a2e6a6adde500a3656b8da3ac5bbc1c9512f030f
Lux <64970263+lumx7@users.noreply.github.com>
2023-07-08 18:37:12 -0400

    ui

--- Gun.gd
+++ Gun.gd
@@ -11,7 +11,6 @@ func _process(_delta):
 	angleTo = lerp_angle(global_rotation, angleTo, 1.0)
 	angleTo = clamp(angleTo, global_rotation - 1, global_rotation + 1)
 	set_global_rotation(angleTo)
-	print(angleTo)
 	if angleTo > -PI/2 and angleTo < PI/2:
 		get_node("Sprite2D").set_flip_v(false)
 	else: 
