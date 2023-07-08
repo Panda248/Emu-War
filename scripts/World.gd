39b3f118a40ba2de9cde23b2a34d0a8ef8c970f9
Lux <64970263+lumx7@users.noreply.github.com>
2023-07-08 10:16:36 -0400

    betterswapping

--- scripts/World.gd
+++ scripts/World.gd
@@ -1,13 +1,22 @@
 extends Node2D
 
+var currentForm
+# 0 = Player, 1 = Bullet
+
 var player
 
 func _ready():
-	player = $Bullet
+	player = $Player
+	currentForm = 0
 
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
-		$Bullet.position.x = $Player/Gun/Point.global_position.x
-		$Bullet.position.y = $Player/Gun/Point.global_position.y
-		player = $Bullet
-		player.visible = true
+		if currentForm == 0:
+			currentForm = 1
+			$Bullet.position = $Player/Gun/Point.global_position
+			player = $Bullet
+			$Bullet.visible = true
+		else:
+			currentForm = 0
+			player = $Player
+			$Bullet.visible = false
