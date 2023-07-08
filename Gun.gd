15f97e2becd01463acb6f4fb99c245fc84e90e75
Panda248 <p4nda248@gmail.com>
2023-07-08 11:03:40 -0400

    rename world to level

--- Gun.gd
+++ Gun.gd
@@ -6,7 +6,7 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var angleTo = global_position.direction_to(get_global_mouse_position()).angle()
 	angleTo = lerp_angle(global_rotation, angleTo, 1.0)
 	angleTo = clamp(angleTo, global_rotation - 1, global_rotation + 1)
