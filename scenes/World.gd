2174659cbfd22ec61404bf426d91fb010a7612da
Panda248 <p4nda248@gmail.com>
2023-07-09 14:15:37 -0400

    1

--- scenes/World.gd
+++ scenes/World.gd
@@ -3,10 +3,14 @@ extends Node2D
 
 var player
 
+var id
+
 func _ready():
 	player = $Player
 
 func _process(delta):
+	if Input.is_action_just_released("ui_accept"):
+		enter_level()
 	#dddddddddddd$CanvasLayer/Label.global_position = find_child("Player").global_position
 	pass
 
@@ -24,24 +28,41 @@ func _on_timer_timeout():
 	pass # Replace with function body.
 	
 func _on_level_1_body_entered(body):
-	$Level1/CollisionShape2D.set_deferred("disabled", true)
-	GlobalVar.world.pack(get_owner())
-	print("1")
-	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	if body is Player:
+		id = 1
+		$Level1/Label.show()
 	pass # Replace with function body.
 
+
+func enter_level():
+	GlobalVar.world.pack(get_owner())
+	get_tree().change_scene_to_file("res://scenes/Level"+ str(id) + ".tscn")
+	
 
 func _on_level_2_body_entered(body):
-	$Level2/CollisionShape2D.set_deferred("disabled", true)
-	GlobalVar.world.pack(get_owner())
-	print("3")
-	get_tree().change_scene_to_file("res://scenes/Level3.tscn")
-	pass # Replace with function body.
+	if body is Player:
+		id = 2
+		$Level2/Label.show()
+		pass # Replace with function body.
 
 
 func _on_level_3_body_entered(body):
-	$Level3/CollisionShape2D.set_deferred("disabled", true)
-	GlobalVar.world.pack(get_owner())
-	print("2")
-	get_tree().change_scene_to_file("res://scenes/Level2.tscn")
+	if body is Player:
+		id = 3
+		$Level3/Label.show()
+	pass # Replace with function body.
+
+
+func _on_level_1_body_exited(body):
+	$Level1/Label.show()
+	pass # Replace with function body.
+
+
+func _on_level_2_body_exited(body):
+	$Level2/Label.show()
+	pass # Replace with function body.
+
+
+func _on_level_3_body_exited(body):
+	$Level3/Label.show()
 	pass # Replace with function body.
